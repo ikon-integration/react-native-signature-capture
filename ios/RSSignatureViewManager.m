2674911dc1c8ae77cf463dc20740028faae6a36d
Gabriel Debes <gabriel.wady@gmail.com>
2019-06-13 13:28:17 -0300

    amend to enabled property on ios

--- ios/RSSignatureViewManager.m
+++ ios/RSSignatureViewManager.m
@@ -13,6 +13,7 @@ RCT_EXPORT_VIEW_PROPERTY(square, BOOL)
 RCT_EXPORT_VIEW_PROPERTY(showBorder, BOOL)
 RCT_EXPORT_VIEW_PROPERTY(showNativeButtons, BOOL)
 RCT_EXPORT_VIEW_PROPERTY(showTitleLabel, BOOL)
+RCT_EXPORT_VIEW_PROPERTY(enabled, BOOL)
 
 
 -(dispatch_queue_t) methodQueue
