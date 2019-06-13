76da5593ce1dd9afbc52edd3e5a0608186214ff2
Gabriel Debes <gabriel.wady@gmail.com>
2019-06-13 15:59:47 -0300

    Fix set disabled on initialization

--- ios/RSSignatureView.m
+++ ios/RSSignatureView.m
@@ -66,6 +66,7 @@
 		sign = [[PPSSignatureView alloc]
 						initWithFrame: CGRectMake(0, 0, screen.width, screen.height)
 						context: _context];
+		[sign setEnabled:_enabled];
 		sign.manager = manager;
 
 		[self addSubview:sign];
